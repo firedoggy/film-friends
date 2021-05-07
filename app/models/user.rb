9fa848e42a0cf63e85b37589a78062aa205cb44f
Samuel <sammkenzie@gmail.com>
2021-05-07 12:26:08 -0500

    Added validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
     has_many :friends, through: :friendships
     has_many :reviews
     has_many :movies, through: :reviews
+    has_secure_password
+    validates :email, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
