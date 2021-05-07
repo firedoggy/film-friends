a1b63e4cef9daa9abcf1f66df60e85b927c2c6ee
Samuel <sammkenzie@gmail.com>
2021-05-07 12:42:35 -0500

    Added MoviesController#index and MoviesController#show.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+
+    def index
+        @movies = Movie.all 
+    end
+
+    def show
+        @movie = Movie.find_by_id(params[:id])
+        redirect_to movies_path if !@movie
+    end
+
 end
