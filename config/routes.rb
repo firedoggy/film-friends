77b39bb273568898bd7f16eebcd468c6fe351373
Samuel <sammkenzie@gmail.com>
2021-05-07 12:28:41 -0500

    Added custom routes for signup, login, and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
-  resources :reviews
-  resources :movies
-  resources :users
-  resources :friendships
+    root 'sessions#home'
+
+    get '/signup' => 'users#new'
+    post '/signup' => 'users#create'
+
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    post '/logout' => 'sessions#destroy'
+
+    resources :reviews
+    resources :movies
+    resources :users
+    resources :friendships
 
 end
