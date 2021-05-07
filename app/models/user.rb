419a2303f9c6520184f5eb3743ac6951307a57ab
Samuel <sammkenzie@gmail.com>
2021-05-07 12:15:17 -0500

    Added associations in User, Movie, and Review models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     
     has_many :friendships, dependent: :destroy  
     has_many :friends, through: :friendships
+    has_many :reviews
+    has_many :movies, through: :reviews
 
 end
