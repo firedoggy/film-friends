8dfd460163258028e3a08ebda5f4a7fad2cbbade
Samuel <sammkenzie@gmail.com>
2021-05-07 12:30:12 -0500

    Added SessionsController#create and SessionController#destroy.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,21 @@ class SessionsController < ApplicationController
 
     def home
     end
+
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+          log_in(user)
+          redirect_to user_path(user)
+        else
+          flash[:message] = "Incorrect login info, please try again"
+          redirect_to '/login'
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
     
 end
