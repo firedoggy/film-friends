14577b1126b5f644d5bc8e45c4d75d26edc3e17d
Samuel <sammkenzie@gmail.com>
2021-05-07 12:27:18 -0500

    Added UsersController#user_params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,11 @@ class UsersController < ApplicationController
             render :new
         end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
     
 end
