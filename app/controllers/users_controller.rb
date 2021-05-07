285ee1845a036562b0da2e5256de4c031abc6746
Samuel <sammkenzie@gmail.com>
2021-05-07 12:27:46 -0500

    Added UsersController#show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,16 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
+    end
+
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:email, :password)
     end
-    
+
 end
