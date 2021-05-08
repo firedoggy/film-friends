106016718bfa677bab31a8ca5eefc33b22b78e54
Samuel <sammkenzie@gmail.com>
2021-05-08 13:50:28 -0500

    Built #new, #create, and #update in ReviewsController.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,17 +9,35 @@ class ReviewsController < ApplicationController
     end
 
     def new
-        @review = Review.new
+        if params[:movie_id] && @movie = Movie.find_by_id(params[:movie_id])
+            @review = @movie.reviews.build
+        else
+            @error = "That movie does not exist." if params[:movie_id]
+            @review = Review.new
+        end
     end
 
     def create
-        @review = Review.ner(reivew)
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to reviews_path
+        else
+            render :new
+        end
+    end
+
+    def update
+        if @review.update(review_params)
+            redirect_to review_path(@review)
+        else
+            render :edit
+        end
     end
 
     private
 
     def review_params()
-        params.require(:review).permit(:content)
+        params.require(:review).permit(:content, :movie_id)
     end
-    
+
 end
